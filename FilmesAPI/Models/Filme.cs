7fa865e70ee244e44864912914f53772aa817452
vivianevianaa <vivianeviana59@gmail.com>
2021-10-10 00:37:49 -0300

    classes Filme e FilmeController

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo título não pode estar em branco.")]
+        public String Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor não pode estar em branco.")]
+        public String Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero deve ter até 30 caracteres.")]
+        public String Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve estar entre 1 e 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
